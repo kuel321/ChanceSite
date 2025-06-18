c4b35e9c1233ae3001aafb478ce9b02788c6601c
kuel321 <lukeshort321@gmail.com>
2025-06-18 17:30:56 -0400

    updated build

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    allowedHosts: ['ec2-pi.local'],
-  },
-})
+  build: {
+    outDir: '/home/luke/chance/dist',
+    emptyOutDir: true
+  }
+});
